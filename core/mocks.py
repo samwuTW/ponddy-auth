310ce88c83604a2c4b19e4cf35aa633d6503ff23
lambdaTW <lambda@lambda.tw>
2019-07-05 13:08:15 +0800

    Add test for the SSO authentication and permissions validation

---
+++ core/mocks.py
@@ -0,0 +1,4 @@
+class MockAuthHTTPResponse():
+    def __init__(self, ok=True, content=''):
+        self.ok = ok
+        self.content = content
---
+++ core/tests.py
@@ -0,0 +1,110 @@
+import json
+from jose import jwt
+
+from unittest import TestCase
+from unittest.mock import patch
+
+from django.conf import settings
+from django.http import HttpRequest
+from django.shortcuts import reverse
+from django.contrib.auth.models import Group, Permission
+
+from ponddy_api_test_client import SSOClient
+from ponddy_auth.authentication import SSOAuthentication
+
+from .mocks import MockAuthHTTPResponse
+
+
+SSO_AUTH_HEADER_PREFIX = getattr(settings, "SSO_AUTH_HEADER_PREFIX", "SSO")
+API_AGENT_PREFIX = getattr(settings, 'API_AGENT_PREFIX', 'api_agent')
+
+
+class SSOAuthenticationTest(TestCase):
+    EMAIL = 'abc@abc.xyz'
+    API = 'Ponddy'
+    SECRET = 'SECRET'
+
+    def get_payload(self):
+        return {
+            'email': self.EMAIL,
+            'api': self.API
+        }
+
+    def setUp(self):
+        self.api, _ = Group.objects.get_or_create(
+            name="{prefix}_{api_agent}".format(
+                prefix=API_AGENT_PREFIX, api_agent=self.API
+            )
+        )
+        self._permission = 'auth.view_user'
+        self._perm_app_label, self._perm_codename = 'auth.view_user'.split('.')
+        self.permission = Permission.objects.get(
+            codename=self._perm_codename,
+            content_type__app_label=self._perm_app_label,
+        )
+        self.api.permissions.add(self.permission)
+        self.token = jwt.encode(self.get_payload(), self.SECRET)
+        self.sso_client = SSOClient(
+            token='{prefix} {token}'.format(
+                prefix=SSO_AUTH_HEADER_PREFIX,
+                token=self.token
+            )
+        )
+
+    @patch('ponddy_auth.authentication.requests.get')
+    def test_sso_auth_will_return_user_if_auth_valid_token(self, mock_auth):
+        mock_auth.side_effect = lambda *arg, **kwargs: MockAuthHTTPResponse(
+            content=json.dumps(self.get_payload())
+        )
+        request = HttpRequest()
+        request.META = self.sso_client.get(reverse('user-list')).request
+        user, payload = SSOAuthentication().authenticate(request)
+
+        assert user.is_anonymous is False
+        self.assertEqual(
+            payload, self.get_payload()
+        )
+
+        assert hasattr(user, "_api_agent")
+        api_agent = getattr(user, "_api_agent")
+        assert isinstance(api_agent, Group)
+
+        assert hasattr(api_agent, "has_perm")
+        assert hasattr(api_agent, "has_perms")
+
+        assert api_agent.has_perm(self._permission)
+        assert api_agent.has_perms([self._permission, ])
+
+    @patch('ponddy_auth.authentication.requests.get')
+    def test_valid_sso_can_list_users(self, mock_auth):
+        mock_auth.side_effect = lambda *arg, **kwargs: MockAuthHTTPResponse(
+            content=json.dumps(self.get_payload())
+        )
+        resp = self.sso_client.get(reverse('user-list'))
+        self.assertEqual(resp.status_code, 200)
+
+    @patch('ponddy_auth.authentication.requests.get')
+    def test_valid_sso_cannot_create_user(self, mock_auth):
+        mock_auth.side_effect = lambda *arg, **kwargs: MockAuthHTTPResponse(
+            content=json.dumps(self.get_payload())
+        )
+        resp = self.sso_client.post(
+            reverse('user-list'),
+            json.dumps(
+                {
+                    'username': 'newuser',
+                    'password': 'TheA11newPWD'
+                }
+            )
+        )
+        self.assertEqual(resp.status_code, 403)
+        self.assertNotEqual(resp.status_code, 200)
+
+    @patch('ponddy_auth.authentication.requests.get')
+    def test_invalid_sso_cannot_do_anything(self, mock_auth):
+        mock_auth.side_effect = lambda *arg, **kwargs: MockAuthHTTPResponse(
+            ok=False,
+            content=b''
+        )
+        resp = self.sso_client.get(reverse('user-list'))
+        self.assertEqual(resp.status_code, 403)
