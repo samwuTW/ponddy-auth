7c1293ff3c943bdb308d0b09f28e41055166ed6a
lambdaTW <lambda@lambda.tw>
2021-04-22 10:37:59 +0800

    Intgrate with pydocstyle

--- src/core/views.py
+++ src/core/views.py
@@ -7,9 +7,7 @@ from .serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
+    """API endpoint that allows users to be viewed or edited."""
 
     queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
